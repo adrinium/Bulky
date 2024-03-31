e2360c877616c223a646935c4adf84e8c7c91c45
Adrian X <adrinium@gmail.com>
2024-03-31 08:56:27 +0300

    Database Context

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public ICollection<Category> Categories { get; set; }
+    }
+}
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public int DisplayOrder { get; set; }
 
     }
